715ff6c0fd04e779e39b50686b04d47ecfbc3ba6
Анастасия Сапожникова <nastyasap53@gmail.com>
2022-09-02 13:01:51 +0300

    burger navbar

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import s from './App.module.scss';
-import {Header} from "./components/header/Header";
 import {Main} from "./components/main/Main";
 import {Contacts} from "./components/contacts/Contacts";
 import {Footer} from "./components/footer/Footer";
 import {Skills} from "./components/skills/Skills";
 import {Projects} from "./components/projects/Projects";
+import {NavBar} from "./components/header/navBar/NavBar";
 
 function App() {
     return (
         <div className={s.App}>
-            <Header/>
+            <NavBar/>
             <Main/>
             <Skills/>
             <Projects/>
--- src/components/header/navBar/NavBar.js
+++ src/components/header/navBar/NavBar.js
@@ -1,14 +1,28 @@
+import {useState} from "react";
+import cn from "classnames"
+
 import s from './NavBar.module.scss'
 import {MenuLink} from "../../../common/Link/MenuLink/MenuLink";
+import burgerMenu from "../../../assets/icons/burgerMenu.svg";
+import cross from "../../../assets/icons/cross.svg";
 
 export function NavBar() {
+    const [isOpen, setIsOpen] = useState(false)
+
     return (
-        <div className={s.nav}>
-            <MenuLink name={'About'} href={'main'}/>
-            <MenuLink name={'Skills'} href={'skills'}/>
-            <MenuLink name={'Projects'} href={'projects'}/>
-            <MenuLink name={'Contacts'} href={'footer'}/>
+        <div className={s.wrapper}>
+            {isOpen ?
+                <img className={s.btn} src={cross} onClick={() => setIsOpen(false)}/>
+                :
+                <img className={s.btn} src={burgerMenu} onClick={() => setIsOpen(true)}/>}
+            <div className={cn(s.nav, {[s.isOpen]: isOpen}) }>
+                <MenuLink name={'About'} href={'main'}/>
+                <MenuLink name={'Skills'} href={'skills'}/>
+                <MenuLink name={'Projects'} href={'projects'}/>
+                <MenuLink name={'Contacts'} href={'footer'}/>
+            </div>
         </div>
-    );
+    )
+        ;
 }
 
