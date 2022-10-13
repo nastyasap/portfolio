31b14a816891c49a9461aff4ef71232d328cc9b9
Анастасия Сапожникова <nastyasap53@gmail.com>
2022-10-13 15:55:27 +0300

    project added

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -6,6 +6,7 @@ import packs from "../../assets/images/projects/packs.png"
 import socNet from "../../assets/images/projects/social.png"
 import todo from "../../assets/images/projects/todo.png"
 import salary from "../../assets/images/projects/salaries.png"
+import crypto from "../../assets/images/projects/crypto.png"
 
 export function Projects() {
     return (
@@ -15,12 +16,15 @@ export function Projects() {
                 <Project img={todo} link={'https://nastyasap.github.io/myTodolist'}
                          title={'To do List'}
                          description={"Make the list of tasks to not forget about something. It's a react application written on typescript with redux. I use redux toolkit, axios and saga. Tests on reducers are written"}/>
-                <Project img={socNet} link={'https://nastyasap.github.io/Social-Network/#/profile'}
-                         title={'Social Network'}
-                         description={"It's a typical social network written on typescript. I used redux, axios, websocket."}/>
                 <Project img={salary} link={'https://yandex-salaries.herokuapp.com/drivers'}
                          title={'Count Salary'}
                          description={"The application for integrating Yandex Taxi with my previous company's internal product to simplify work with large amounts of data. It's react application written on javascript with redux."}/>
+                <Project img={crypto} link={'https://nastyasap.github.io/coincap_test/#/'}
+                         title={'Cryptocurrencies'}
+                         description={'List of al cryptocurrencies, information about every item and possibility of adding it to your wallet. Stack: React, Redux, Typescript, Axios, redux-saga, redux-toolkit, styled-components, react-router-dom, redux-persist, recharts. '}/>
+                <Project img={socNet} link={'https://nastyasap.github.io/Social-Network/#/profile'}
+                         title={'Social Network'}
+                         description={"It's a typical social network written on typescript. I used redux, axios, websocket."}/>
                 <Project img={packs} link={'https://Ivan0195.github.io/FridayProjectTest'}
                          title={'Card Packs'}
                          description={'Create a card pack to memorize something. The process of learning includes your previous answers. Also you can see public packs of other users. This project was made by the team consists of 3 people. It\'s a react application written on typescript with redux.'}/>
